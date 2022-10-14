7a20a3dcbb179f8ea5b506ca3cb1d5e89a9d9b53
Acheampong Stephen <acheampongstephen392024@gmail.com>
2022-10-14 10:39:13 -0700

    pytest

--- test/test_config.py
+++ test/test_config.py
@@ -12,4 +12,15 @@ def test_generic():
     with pytest.raises(NotInRange):
         if a not in range(10, 20):
             raise NotInRange
+
+#It should be noted that if you want to apply a test to a specific fuction,
+#always make sure to add the the first key words to the function
+
+def test_something():
+    a = 2
+    b = 2
+    assert True #Comparison to ensure if True/false
+
+
+
     
